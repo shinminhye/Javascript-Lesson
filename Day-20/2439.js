21ffe3ec97e91fa1f176eaba4ffc765b35f7f3c7
Your Name <you@example.com>
2019-11-11 22:31:22 +0900

    Day 31

--- Day-20/2439.js
+++ Day-20/2439.js
@@ -28,20 +28,97 @@ function write(string) {
     fs.writeSync(process.stdout.fd, buffer, 0, buffer.length);
 }
 
-function main() {
+function main2() {
+    // 입력으로 주어지는 자연수 문자열
     let input = readInput(); // 10 10
     /********** */
     // console.log(input);
+    // 입력을 숫자로 변환한 값
     const N = Number(input);
+    // 초기값을 1, i= n보다 작거나 같은동안, 1씩 증가한다. i= 1~n번까지 반복한다.
     for (let i = 1; i <= N; i++) {
-        for (let s = 1; s<=N-i; s++) {
+        //s는 초기값1, s는 n-i보다 작거나 같은동안, s= 1씩 증가한다.
+        //s는 1 ~ n-i보다 작거나 같은동안 반복한다.
+        for (let s = 1; s <= N - i; s++) {
+            //공백을 쓴다.
             write(' ');
         }
+        //j는초기값1, j는 i보다작거나 같은동안, j는 1씩 증가한다.
         for (let j = 1; j <= i; j++) {
+            // *를 찍는다.
             write('*');
         }
+
+        //줄바꿈을 한다.
         write('\n');
     }
 }
 
+/**
+ * n=?
+i=*
+j=''
+l = 1~5
+
+5-3=2
+n-i=j
+
+n-(1~n)=j
+
+?-(1-10)=-9
+
+
+=================
+
+N => 전체 줄수
+L => 현재 줄
+
+=========
+
+i => '*' 개수
+j => ' ' 개수
+
+=========
+i = L
+i + j = N
+ => L + j = N
+ => L + j - L = N - L
+ => j = N - L
+
+j = N - L
+=========
+js=============
+function printN(ch, repeat);
+// printN(' ', 10);
+function printLine(N, L);
+
+function program(N);
+
+function main();
+ */
+
+function repeatPrintChar(char, repeat) {
+    // repeat ->  ch
+    for (let i = 0; i < repeat; i++) {
+        write(char);
+    }
+}
+
+function printLine(N, L) {
+    // countSpace = ?
+    // countStar = ?
+    // repeatPrintChar ' ' countSpace
+    // repeatPrintChar '*' countStar
+    // write '\n'
+}
+
+function program(N) {
+    // L => 1 ... N
+    // printLine N L
+}
+
+function main() { }
+
 main();
+
+// 2019-11-11 RE
--- Day-29-30/app.js
+++ Day-29-30/app.js
@@ -24,7 +24,7 @@ display.innerHTML = '0';
 // 3. 상태 정보 저장 공간, store 정의
 const store = {
     x: 0,
-    ops: '.',
+    ops: '.', // . => 선택전, +,-,*,/ => 사칙연산
     y: 0
 }
 // 4. store를 조작하는 함수 정의/구현
@@ -64,11 +64,67 @@ document.querySelector("[x-data-pad-btn='D8']").onclick = function () { onClickN
 document.querySelector("[x-data-pad-btn='D9']").onclick = function () { onClickNumber(9); }
 document.querySelector("[x-data-pad-btn='D0']").onclick = function () { onClickNumber(0); }
 
-// ==============================
+const arithmeticBtns = Array.from(document.querySelectorAll('[x-data-pad-btn="AM"],[x-data-pad-btn="AD"],[x-data-pad-btn="AS"],[x-data-pad-btn="AP"]'));
 
-const btnMultiply = document.querySelector('[x-data-pad-btn="AM"]');
-
-function onBtnMultiplyClick(){
-    btnMultiply.style.backgroundColor = 'red';
+function clearSelectedStyle() {
+    arithmeticBtns.forEach(btn => {
+        btn.style.backgroundColor = '';
+    });
 }
-btnMultiply.onclick = onBtnMultiplyClick // function () { onBtnMultiplyClick(); }
+
+function arrHasElement(arr, element) {
+    return arr.indexOf(element) !== -1;
+}
+
+function onArithmeticClick(e) {
+    // console.log(e);
+    let i = 0
+    let target;
+
+    // do {
+    //     target = e.path[i];
+    //     i += 1;
+    // } while (!arrHasElement(arithmeticBtns, target));
+
+    while (true) {
+        target = e.path[i];
+        i += 1;
+
+        const targetIsInArr = arrHasElement(arithmeticBtns, target);
+        if (targetIsInArr) {
+            break;
+        } else {
+            continue;
+        }
+    }
+    //} while(arithmeticBtns.indexOf(target)===-1);
+
+    const action = target.getAttribute('x-data-pad-btn');
+    if (action === 'AD') {
+        store.ops = '/';
+    }
+    if (action === 'AP') {
+        store.ops = '+';
+    }
+    if (action === 'AM') {
+        store.ops = '*';
+    }
+    if (action === 'AS') {
+        store.ops = '-';
+    }
+    // console.log(store);
+
+    clearSelectedStyle();
+    target.style.backgroundColor = 'red';
+}
+
+arithmeticBtns.forEach(btn => {
+    btn.onclick = onArithmeticClick;
+});
+
+
+// const clickedElement = {};
+// const event = {
+//     target: clickedElement
+// };
+// clickedElement.onclick(event);
